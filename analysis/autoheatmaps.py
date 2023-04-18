3dbf3ea751ea74c61dbdcc1e079d41598b7b578b
neurotronk <ajvalpe@gmail.com>
2023-04-18 10:25:16 +0200

    [st] code for analysis

--- analysis/autoheatmaps.py
+++ analysis/autoheatmaps.py
@@ -8,38 +8,9 @@ import matplotlib.pyplot as plt
 from scipy import signal
 import pyxdf
 import matplotlib
-matplotlib.use('QtAgg')
+# matplotlib.use('QtAgg')
+import commons as cm
 
-
-def do_heatmap(image,gaze_on_surf_x,gaze_on_surf_y):
-    grid = image.shape[0:2] # height, width of the loaded image
-    heatmap_detail = 0.02 # this will determine the gaussian blur kerner of the image (higher number = more blur)
-    # flip the fixation points
-    # from the original coordinate system,
-    # where the origin is at botton left,
-    # to the image coordinate system,
-    # where the origin is at top left
-    gaze_on_surf_y = 1 - gaze_on_surf_y
-
-    # make the histogram
-    hist, x_edges, y_edges = np.histogram2d(
-        gaze_on_surf_y,
-        gaze_on_surf_x,
-        range=[[0, 1.0], [0, 1.0]],
-        bins=grid
-    )
-
-    # gaussian blur kernel as a function of grid/surface size
-    filter_h = int(heatmap_detail * grid[0]) // 2 * 2 + 1
-    filter_w = int(heatmap_detail * grid[1]) // 2 * 2 + 1
-    heatmap = gaussian_filter(hist, sigma=(filter_w, filter_h), order=0)
-
-    # display the histogram and reference image
-    print("Cover image with heatmap overlay")
-    plt.figure(figsize=(8,8))
-    plt.imshow(image)
-    plt.imshow(heatmap, cmap='jet', alpha=0.5)
-    plt.axis('off');
 
 
 def main():
@@ -78,21 +49,20 @@ def main():
     gaze_on_surf=gaze_pd_frame[(gaze_pd_frame.on_surf==True)&(gaze_pd_frame.confidence >conf_thr)]
 
     for im,im_order in zip(images, image_order):
-        print(im,im_order)
-        index_stim=annotations_pd['label']==im
-        index_final=annotations_pd.index[index_stim]+1
 
-        value=annotations_pd[index_stim]['timestamp'].values[0]
-        value_final=annotations_pd.iloc[index_final]['timestamp'].values[0]
-        gaze_on_surf_im=gaze_on_surf[
-            (gaze_on_surf['gaze_timestamp']>value)&
-            (gaze_on_surf['gaze_timestamp']<value_final)
-        ]
+        initial_anotation,end_anotation=cm.extract_annotations_timestamps(im,'label',annotations_pd)
+
+        gaze_on_surf_im=cm.filter_rows_by_temporal_values(
+            dataframe=gaze_on_surf,
+            time_column='gaze_timestamp',
+            ini_value=initial_anotation['timestamp'].values[0],
+            end_value=end_anotation['timestamp'].values[0]
+        )
+
         image=plt.imread(image_location+'/'+im_order)
-        do_heatmap(image,gaze_on_surf_im['x_norm'],gaze_on_surf_im['y_norm'])
-        plt.savefig('heatmaps/'+im_order,dpi=80)
+        cm.do_heatmap(image,gaze_on_surf_im['x_norm'],gaze_on_surf_im['y_norm'])
+        plt.savefig('heatmaps/'+im_order)
         plt.close()
-
 
 if __name__ == '__main__':
     try:
--- analysis/commons.py
+++ analysis/commons.py
@@ -20,3 +20,71 @@ def plot_gaze_eyes(x_0,y_0,x_1,y_1):
     ax.set_xlabel('x coordinates')
     ax.set_ylabel('y coordinates')
     fig.set_figwidth(10)
+
+
+def extract_annotations_timestamps(annotation,annotation_col,dataframe):
+    """Function to extract timesatmps from annotations table of pupil core
+       It basically serches for the annotation field in a datraframe and returns
+       the rows of the annotation and of the next event
+
+    Args:
+        annotation (_str_): string of the annotation of interest
+        annotation_col(_str_): column name of the annotation
+
+    """
+    index_stim=dataframe[annotation_col]==annotation
+    index_final=dataframe.index[index_stim]+1
+
+    start_row=dataframe[index_stim]
+    end_row=dataframe.iloc[index_final]
+
+    return start_row, end_row
+
+def filter_rows_by_temporal_values(dataframe,time_column,ini_value,end_value):
+    """Common funtion to return a segment of a dataframe filtered by the timestamps
+      greater than or equal to a column value and less than another timestamp values.
+
+    Args:
+        dataframe (_pandas dataframe_): dataframe of interest 
+        time_column (_type_): column to use for filtering
+        ini_value (_type_): initial temporal value
+        end_value (_type_): final temporal value
+    """
+        
+    segmented_df=dataframe[
+        (dataframe[time_column]>=ini_value)&
+        (dataframe[time_column]< end_value)
+    ]
+    return segmented_df
+
+def do_heatmap(image,gaze_on_surf_x,gaze_on_surf_y):
+    from scipy.ndimage.filters import gaussian_filter
+
+    grid = image.shape[0:2] # height, width of the loaded image
+    heatmap_detail = 0.02 # this will determine the gaussian blur kerner of the image (higher number = more blur)
+    # flip the fixation points
+    # from the original coordinate system,
+    # where the origin is at botton left,
+    # to the image coordinate system,
+    # where the origin is at top left
+    gaze_on_surf_y = 1 - gaze_on_surf_y
+
+    # make the histogram
+    hist, x_edges, y_edges = np.histogram2d(
+        gaze_on_surf_y,
+        gaze_on_surf_x,
+        range=[[0, 1.0], [0, 1.0]],
+        bins=grid
+    )
+
+    # gaussian blur kernel as a function of grid/surface size
+    filter_h = int(heatmap_detail * grid[0]) // 2 * 2 + 1
+    filter_w = int(heatmap_detail * grid[1]) // 2 * 2 + 1
+    heatmap = gaussian_filter(hist, sigma=(filter_w, filter_h), order=0)
+
+    # display the histogram and reference image
+    print("Cover image with heatmap overlay")
+    plt.figure(figsize=(8,8))
+    plt.imshow(image)
+    plt.imshow(heatmap, cmap='jet', alpha=0.5)
+    plt.axis('off');
